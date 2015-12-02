74195deed08812cb2ac004753933b4334b62a1d5
nnedevn <nedevn@gmail.com>
2015-12-02 15:55:59 -0800

    Adds simple HTML5 form validation and reverts let to var. Let does not work in Safari.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,15 +6,15 @@ console.log ('app.js');
 
 // this code is must be revised
 
-    let $windows = $('#about,#code-samples,#projects,#contact');
-    let $buttons = $('#nav-about,#nav-code-samples,#nav-projects, #nav-contact');
+    var $windows = $('#about,#code-samples,#projects,#contact');
+    var $buttons = $('#nav-about,#nav-code-samples,#nav-projects, #nav-contact');
 
     $('#about').show();
 
     $buttons.on('click', function(e){
         var $id;
 
-        let $divid = $('#' + this.id.substring(4));
+        var $divid = $('#' + this.id.substring(4));
 
     if(!$divid.is(':visible') ) {
         $windows.slideUp();
